75e7ee4fc4a96aa448612b09a3f8bea457ad9c2a
marcus-elia <mselia@uvm.edu>
2021-11-10 22:37:22 -0500

    Added game flow and basic bad guys

--- Doors of Doom/Assets/Scripts/DoorManager.cs
+++ Doors of Doom/Assets/Scripts/DoorManager.cs
@@ -2,25 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum GameState { Choosing, LeftChosen, RightChosen };
+public enum GameState { Choosing, PlayerMoveLeft, PlayerMoveRight, PlayerMoveForward, PlayerInHallway };
 
 public class DoorManager : MonoBehaviour
 {
-    public float roomWidth_ = 10f;
-    public float roomDepth_ = 10f;
-    public float roomHeight_ = 10f;
+    // Physical setup of the scene
+    private static float roomWidth_ = 10f;
+    private static float roomDepth_ = 10f;
+    private static float roomHeight_ = 10f;
 
-    public float hallwayWidth_ = 4f;
-    public float hallwayDepth_ = 10f;
-    public float hallwayHeight_ = 7f;
+    private static float hallwayWidth_ = 4f;
+    private static float hallwayDepth_ = 10f;
+    private static float hallwayHeight_ = 7f;
 
+    private static float leftDoorX = -2.5f;
+    private static float rightDoorX = 2.5f;
+    private static float hallwayEndZ = roomDepth_/2 + hallwayDepth_ - 2f;
+
+    private static Vector3 leftBadGuySpawn = new Vector3(leftDoorX, 3, hallwayEndZ - 1);
+    private static Vector3 rightBadGuySpawn = new Vector3(rightDoorX, 3, hallwayEndZ - 1);
+
+    // Materials
     public Material stoneBrickUncolored;
     public Material woodDoorUncolored;
     public Material darkness;
 
     private GameObject doorWall_, leftDoor_, rightDoor_, leftHallway_, rightHallway_;
 
+    // Game Management
     private GameState currentState = GameState.Choosing;
+    private int level = 0;
+
+    public Transform playerTransform;
+    private Vector3 playerStartPosition_ = new Vector3(0, 1.5f, -4);
+    public float playerLateralSpeed = 0.002f;
+    public float playerForwardSpeed = 0.005f;
+
+    // Other prefabs
+    public GameObject badGuyPrefab;
+    private GameObject currentBadGuy_;
 
     // Start is called before the first frame update
     void Start()
@@ -83,18 +103,90 @@ public class DoorManager : MonoBehaviour
             {
                 rightDoor_.GetComponent<Door>().Toggle();
             }
+
+            // Check if a door is open all the way
+            if (leftDoor_.GetComponent<Door>().IsOpen())
+            {
+                currentState = GameState.PlayerMoveLeft;
+                rightDoor_.GetComponent<Door>().Close();
+            }
+            else if (rightDoor_.GetComponent<Door>().IsOpen())
+            {
+                currentState = GameState.PlayerMoveRight;
+                leftDoor_.GetComponent<Door>().Close();
+            }
         }
-        
-        // Check if a door is open all the way
-        if(leftDoor_.GetComponent<Door>().IsOpen())
+        else if (currentState == GameState.PlayerMoveLeft)
         {
-            currentState = GameState.LeftChosen;
-            rightDoor_.GetComponent<Door>().Close();
+            if(playerTransform.position.x > leftDoorX)
+            {
+                playerTransform.Translate(-playerLateralSpeed, 0, 0);
+            }
+            else
+            {
+                playerTransform.position = new Vector3(leftDoorX, playerTransform.position.y, playerTransform.position.z);
+                currentState = GameState.PlayerMoveForward;
+            }
         }
-        else if(rightDoor_.GetComponent<Door>().IsOpen())
+        else if (currentState == GameState.PlayerMoveRight)
         {
-            currentState = GameState.RightChosen;
-            leftDoor_.GetComponent<Door>().Close();
+            if (playerTransform.position.x < rightDoorX)
+            {
+                playerTransform.Translate(playerLateralSpeed, 0, 0);
+            }
+            else
+            {
+                playerTransform.position = new Vector3(rightDoorX, playerTransform.position.y, playerTransform.position.z);
+                currentState = GameState.PlayerMoveForward;
+            }
+        }
+        else if(currentState == GameState.PlayerMoveForward)
+        {
+            playerTransform.Translate(0, 0, playerForwardSpeed);
+
+            if(playerTransform.position.z > roomDepth_ / 2)
+            {
+                leftDoor_.GetComponent<Door>().Close();
+                rightDoor_.GetComponent<Door>().Close();
+                currentState = GameState.PlayerInHallway;
+                SpawnBadGuy();
+            }
+
+            
+        }
+        else if(currentState == GameState.PlayerInHallway)
+        {
+            playerTransform.Translate(0, 0, playerForwardSpeed);
+            if (playerTransform.position.z > hallwayEndZ)
+            {
+                this.SetupNextLevel();
+            }
+        }
+
+
+
+    }
+
+    private void SetupNextLevel()
+    {
+        level++;
+        currentState = GameState.Choosing;
+        playerTransform.position = playerStartPosition_;
+        Destroy(currentBadGuy_);
+
+    }
+
+    private void SpawnBadGuy()
+    {
+        currentBadGuy_ = Instantiate(badGuyPrefab);
+        if(Random.Range(0, 2) == 0)
+        {
+            currentBadGuy_.transform.position = leftBadGuySpawn;
+        }
+        else
+        {
+            currentBadGuy_.transform.position = rightBadGuySpawn;
         }
     }
+    
 }
